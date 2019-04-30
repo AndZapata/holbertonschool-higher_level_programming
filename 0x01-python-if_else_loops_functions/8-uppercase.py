6850d713a9bdcecbed2002b3332846e1617e916e
AndZapata <andzapata514@gmail.com>
2019-04-30 23:09:30 +0000

    [Feature] past to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if str == "":
-        return(ord(str))
     x = 0
     while x < len(str):
         if str[x].islower():
             y = ord(str[x]) - 32
         else:
             y = ord(str[x])
-        if x != len(str) - 1:
+        if x != len(str):
             print("{:s}".format(chr(y)), end="")
-        else:
-            print("{:s}".format(chr(y)))
         x += 1
+    print("")
