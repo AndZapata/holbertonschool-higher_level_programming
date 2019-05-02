89274f673710145464a9d5c76d8331c8cb4613f7
AndZapata <andzapata514@gmail.com>
2019-05-02 23:19:59 +0000

    [Feature] Shows the files inside the compiled file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-list = dir(hidden_4)
-for i in range(len(list)):
-    if not (list[i].startswith("__")):
-        print(list[i])
+if __name__ == "__main__":
+    list = dir(hidden_4)
+    for i in range(len(list)):
+        if not (list[i].startswith("__")):
+            print(list[i])
