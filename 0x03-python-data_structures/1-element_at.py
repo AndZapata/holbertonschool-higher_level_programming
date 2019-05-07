ae511576d5947e26b81ea149e9b28777b3392502
AndZapata <andzapata514@gmail.com>
2019-05-07 19:14:17 +0000

    [Fix] equal

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     i = len(my_list)
     if idx < 0:
         return None
-    elif idx > i:
+    elif idx >= i:
         return None
     else:
         return my_list[idx]
