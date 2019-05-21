4ddf3e1b0305cd77bb1117d17c25fe2fb5dec443
AndZapata <andzapata514@gmail.com>
2019-05-21 23:14:47 +0000

    [Feature] print a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -23,4 +23,6 @@ class Square:
         for i in range(self.__size):
             for j in range(self.__size):
                 print("{}".format('#'), end='')
-            print('')
+            print()
+        if self.__size == 0:
+            print()
