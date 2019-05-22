c4b7f0cbb92fde31733a04f88fe5e290ecb31f29
AndZapata <andzapata514@gmail.com>
2019-05-22 02:06:46 +0000

    [Feature] pep

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,12 +38,13 @@ class Square:
         return self.size ** 2
 
     def my_print(self):
-        for i in range(self.position[1]):
-            print()
-        for i in range(self.size):
-            print(' ' * self.position[0], end='')
-            for i in range(self.size):
-                print("{}".format('#'), end='')
-            print()
         if self.size == 0:
             print()
+        else:
+            for i in range(self.position[1]):
+                print()
+            for i in range(self.size):
+                print(' ' * self.position[0], end='')
+                for i in range(self.size):
+                    print("{}".format('#'), end='')
+                print()
