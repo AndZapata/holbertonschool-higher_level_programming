5b7f1ed580ba579e449b2c7416d70f0f2cfd14cd
AndZapata <778@holbertonschool.com>
2019-05-25 00:07:29 +0000

    [Feature] fix pep8 indentation

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,7 +15,7 @@ def matrix_divided(matrix, div):
     tmp_mat = []
     err = "Each row of the matrix must have the same size"
     err_mes = "matrix must be a matrix (list of lists) of integers/floats"
-    if  type(div) != int and type(div) != float:
+    if type(div) != int and type(div) != float:
         raise TypeError('div must be a number')
     elif div == 0:
         raise ZeroDivisionError('division by zero')
