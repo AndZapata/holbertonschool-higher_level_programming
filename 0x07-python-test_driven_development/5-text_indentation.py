2082522a049955d590eb95b4b47335fe78e3097d
AndZapata <andzapata514@gmail.com>
2019-05-25 20:33:13 +0000

    [Fix] cases

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,9 +3,20 @@
 
 
 def text_indentation(text):
+    """
+    Check the type of the text if it is a string,
+    if not: raise an error message
+    Take the old text and replace the characters
+    '. ', ': ', '? ', with '\n\n'
+    print the newest text with all the changes
+    """
+#!/usr/bin/python3
+""" text_indentation - Function that replace special characters with \n\n """
+
+
+def text_indentation(text):
     lst = []
     list2 = []
-    limiters = ".?:"
     """
     Check the type of the text if it is a string,
     if not: raise an error message
@@ -25,5 +36,3 @@ def text_indentation(text):
             list2.append(i.strip())
         parcial = '\n'.join(list2)
         print('\n'.join(list2))
-        if len(parcial) > 0 and parcial[-1] in limiters:
-            print("")
