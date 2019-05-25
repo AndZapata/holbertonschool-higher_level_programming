cfaebc3875fa570ca229208a2f6bc5022ab46443
AndZapata <andzapata514@gmail.com>
2019-05-25 17:56:03 +0000

    [Fix] cases

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -15,18 +15,22 @@ def matrix_divided(matrix, div):
     tmp_mat = []
     err = "Each row of the matrix must have the same size"
     err_mes = "matrix must be a matrix (list of lists) of integers/floats"
+    if type(matrix) != list or not len(matrix):
+        raise TypeError(err_mes)
+    lon = len(matrix[0])
+    for i in matrix:
+        if type(i) != list or not len(matrix):
+            raise TypeError(err_mes)
+        if lon != len(i):
+            raise TypeError(err)
+        for j in i:
+            if type(j) != int and type(j) != float:
+                raise TypeError(err_mes)
     if type(div) != int and type(div) != float:
         raise TypeError('div must be a number')
     elif div == 0:
         raise ZeroDivisionError('division by zero')
-    else:
-        lon = len(matrix[0])
-        for i in matrix:
-            if lon != len(i):
-                raise TypeError(err)
-            for j in i:
-                if type(j) != int and type(j) != float:
-                    raise TypeError(err_mes)
-                tmp_mat = list(map(lambda j: round((j / div), 2), i))
-            new_mat.append(tmp_mat)
-        return new_mat
+    for i in matrix:
+        tmp_mat = list(map(lambda j: round((j / div), 2), [j for j in i]))
+        new_mat.append(tmp_mat)
+    return new_mat
