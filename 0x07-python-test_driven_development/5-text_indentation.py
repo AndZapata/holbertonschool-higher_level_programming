f9a8eab9f663a307d400f94b8e0fde0194c7f6ad
AndZapata <andzapata514@gmail.com>
2019-05-25 21:07:35 +0000

    [Fix] cases

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,29 +10,10 @@ def text_indentation(text):
     '. ', ': ', '? ', with '\n\n'
     print the newest text with all the changes
     """
-#!/usr/bin/python3
-""" text_indentation - Function that replace special characters with \n\n """
-
-
-def text_indentation(text):
-    lst = []
-    list2 = []
-    """
-    Check the type of the text if it is a string,
-    if not: raise an error message
-    Take the old text and replace the characters
-    '. ', ': ', '? ', with '\n\n'
-    print the newest text with all the changes
-    """
     if type(text) != str:
         raise TypeError('text must be a string')
     else:
-        new_text = text.replace('.', ".\n\n")
-        new_text = new_text.replace(':', ":\n\n")
-        new_text = new_text.replace('?', "?\n\n")
-        new_text = new_text.strip()
-        lst = new_text.split('\n')
-        for i in lst:
-            list2.append(i.strip())
-        parcial = '\n'.join(list2)
-        print('\n'.join(list2))
+        text = text.replace('.', ".\n\n")
+        text = text.replace(':', ":\n\n")
+        text = text.replace('?', "?\n\n")
+        print('\n'.join(i.strip() for i in text.split('\n')), end='')
