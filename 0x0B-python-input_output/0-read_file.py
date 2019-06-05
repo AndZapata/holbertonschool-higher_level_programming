16db7b78c19ed0799b324b925155601378c22c7a
AndZapata <andzapata514@gmail.com>
2019-06-05 16:23:44 +0000

    [Fix] loop for file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def read_file(filename=""):
     with open(filename, encoding='UTF8') as myFile:
-        print(myFile.read(), end='')
+        for line in myFile:
+            print(line, end='')
