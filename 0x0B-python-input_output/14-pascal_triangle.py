b75e7d463cdc3c499ff3bef1615684c0c3d7a458
AndZapata <andzapata514@gmail.com>
2019-06-05 22:49:59 +0000

    [Feature] pascal triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    list1 = [[1], [1,1]]
+    for i in range(1, n - 1):
+        line = [1]
+        for j in range(0, len(list1[i]) - 1):
+            line.extend([list1[i][j] + list1[i][j + 1]])
+        list1.append(line)
+        line += [1]
+    return list1
