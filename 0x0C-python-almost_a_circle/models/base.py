e60107e28559ca73352d6bf052326d2b2df4d699
AndZapata <andzapata514@gmail.com>
2019-06-11 01:47:38 +0000

    [Feature] base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import json
+import csv
 
 
 class Base():
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
@@ -13,11 +15,65 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        if list_dictionaries is None or list_dictionaries == "":
+            return []
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        jason = cls.to_json_string(list_objs)
-        print(jason)
-        with open('{}.json'.format(cls), 'w') as myFile:
-            json.dump(jason, myFile)
+        list_ob2 = []
+        if list_objs is not None:
+            for i in list_objs:
+                list_ob2.append(i.to_dictionary())
+
+        with open('{}.json'.format(cls.__name__), 'w') as File:
+            File.write(cls.to_json_string(list_ob2))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        if cls.__name__ == 'Rectangle':
+            panda_dummy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            panda_dummy = cls(1)
+        panda_dummy.update(**dictionary)
+        return panda_dummy
+
+    @classmethod
+    def load_from_file(cls):
+        list_obj2 = []
+        try:
+            with open('{}.json'.format(cls.__name__), 'r') as File:
+                File2 = cls.from_json_string(File.read())
+                for i in File2:
+                    list_obj2.append(cls.create(**i))
+        except:
+            pass
+        return list_obj2
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        for a in list_objs:
+            dict_1 = http://a.to _dictionary()
+        with open(cls.__name__ + ".csv", 'w') as mycsv:
+            keys = dict_1.keys()
+            pepito = csv.DictWriter(mycsv, keys)
+            pepito.writeheader()
+            pepito.writerow(dict_1)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        juanitolist = []
+
+        with open(cls.__name__ + ".csv", newline='') as mycsv:
+            var = csv.DictReader(mycsv)
+            for row in var:
+                for key, value in row.items():
+                    row[key] = int(value)
+                juanitolist.append(row)
+        return [cls.create(**a) for a in juanitolist]
