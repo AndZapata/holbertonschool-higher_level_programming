148a1209f7d524372a50cb53eceeef0aedd9675e
AndZapata <andzapata514@gmail.com>
2019-06-11 19:33:40 +0000

    [gadshd]

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -4,6 +4,7 @@ import unittest
 import pep8
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestCodeFormat(unittest.TestCase):
@@ -11,91 +12,86 @@ class TestCodeFormat(unittest.TestCase):
 
     def test_pep8_conformance(self):
         """ test pep8 """
-        fchecker = pep8.Checker("models/rectangle.py", show_source=True)
+        fchecker = pep8.Checker("models/square.py", show_source=True)
         file_errors = fchecker.check_all()
 
-class TestRectangleWorking(unittest.TestCase):
+class TestSquareWorking(unittest.TestCase):
     """ class test rectangle """
 
-    def test_rec_normal(self):
+    def test_squ_normal(self):
         """ Normal cases """
-        r1 = Rectangle(10, 2, 0, 0, 1)
-        self.assertEqual(r1.id, 1)
-        r2 = Rectangle(2, 10, 0, 0, 2)
-        self.assertEqual(r2.id, 2)
-        r3 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r3.id, 12)
-        r4 = Rectangle(3, 4, 5, 6, 7)
-        self.assertEqual(r4.height, 4)
-        r5 = Rectangle(1, 2, 3, 4, 8)
-        self.assertEqual(r5.width, 1)
-        self.assertEqual(r5.x, 3)
-        self.assertEqual(r5.y, 4)
-        self.assertEqual(r5.id, 8)
+        s1 = Square(10, 0, 0, 1)
+        self.assertEqual(s1.id, 1)
+        s2 = Square(2, 0, 0, 2)
+        self.assertEqual(s2.id, 2)
+        s3 = Square(2, 0, 0, 12)
+        self.assertEqual(s3.id, 12)
+        s4 = Square(4, 5, 6, 7)
+        self.assertEqual(s4.size, 4)
+        s5 = Square(2, 3, 4, 8)
+        self.assertEqual(s5.size, 2)
+        self.assertEqual(s5.x, 3)
+        self.assertEqual(s5.y, 4)
+        self.assertEqual(s5.id, 8)
 
     def test_arg_one(self):
         """ one argument case """
-        with self.assertRaises(TypeError):
-            r6 = Rectangle(1)
+        s6 = Square(1)
+        self.assertEqual(s6.size, 1)
 
-    def test_width_zero(self):
+    def test_size_zero(self):
         """ 0 width argument case """
         with self.assertRaises(ValueError):
-            r7 = Rectangle(0, 1)
-
-    def test_height_zero(self):
-        """ 0 height argument case """
-        with self.assertRaises(ValueError):
-            r8 = Rectangle(1, 0)
+            s7 = Square(0)
 
     def test_no_args(self):
         """No arguments cases """
         with self.assertRaises(TypeError):
-            r9 = Rectangle()
+            s8 = Square()
 
     def test_x_neg(self):
         """ Negative x """
         with self.assertRaises(ValueError):
-            r10 = Rectangle(1, 2, -1, 2)
+            s9 = Square(1, 2, -1, 8)
 
     def test_y_neg(self):
         """ Negative y """
         with self.assertRaises(ValueError):
-            r11 = Rectangle(1, 2, 1, -1)
+            s10 = Square(1, -2, 1, 1)
 
     def test_width_wr_type(self):
         """ String width """
         with self.assertRaises(TypeError):
-            r12 = Rectangle('A', 2, 1, -1)
+            s11 = Square('A', 1, -1)
 
     def test_height_wr_type(self):
         """ String height """
         with self.assertRaises(TypeError):
-            r13 = Rectangle(1, 'H', 1, 0)
+            s12 = Square(1, 'H', 1, 0)
 
     def test_x_wr_type(self):
         """ String x """
         with self.assertRaises(TypeError):
-            r14 = Rectangle(1, 2, 'A', 0)
+            s13 = Square(1, 2, 'A', 0)
 
     def test_y_wr_type(self):
         """ String y """
         with self.assertRaises(TypeError):
-            r15 = Rectangle(1, 2, 1, 'H')
+            s14 = Square(1, 'h', 1, 0)
 
     def test_float_Rec(self):
         """ float numbers """
         with self.assertRaises(TypeError):
-            r16 = Rectangle(2.5, 3, 4, 5, 9)
+            s15 = Square(2.5, 3, 4, 5, 9)
 
     def test_area_Rec_nor(self):
         """ Test Area """
-        r17 = Rectangle(3, 2)
-        self.assertEqual(r17.area(), 6)
-        r18 = Rectangle(2, 10)
-        self.assertEqual(r18.area(), 20)
-        r19 = Rectangle(8, 7, 0, 0, 12)
-        self.assertEqual(r19.area(), 56)
+        s16 = Square(3)
+        self.assertEqual(s16.area(), 9)
+        s17 = Square(2)
+        self.assertEqual(s17.area(), 4)
+        s18 = Square(8)
+        self.assertEqual(s18.area(), 64)
 
 
 
