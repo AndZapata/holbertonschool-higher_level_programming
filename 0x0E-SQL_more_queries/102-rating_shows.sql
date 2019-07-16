faf5f90188cd769c51096ecadaba36b38cb05481
AndZapata <andzapata514@gmail.com>
2019-07-16 23:00:33 +0000

    [Feature] Third advanced task

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,8 @@
+-- comentario
+-- comentario
+SELECT tv_shows.title,
+SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows JOIN tv_show_ratings
+ON tv_show_ratings.show_id = tv_shows.id
+GROUP BY tv_show_ratings.show_id
+ORDER BY rating DESC;
