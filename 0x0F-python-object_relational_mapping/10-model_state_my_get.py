38999b9aa9f5eb2fc26435156176578157fef1cb
AndZapata <andzapata514@gmail.com>
2019-08-11 17:14:46 +0000

    [Finally] understanding alchemy

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    s = Session()
+    q = s.query(State).filter_by(name =
+        argv[4]).order_by(asc(State.id)).one_or_none()
+    if q:
+        print("{:d}".format(q.id))
+    else:
+        print("Not found")
+    s.close()
