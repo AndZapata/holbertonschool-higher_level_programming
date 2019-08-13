e9553a15562c30fa9c8ada31f052e0a57e888771
AndZapata <andzapata514@gmail.com>
2019-08-13 00:04:03 +0000

    [Feature] Second advanced task

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy import (create_engine)
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    s = Session()
+    q = s.query(State).order_by(asc(State.id)).all()
+    for i in q:
+        print("{}: {}".format(i.id, i.name))
+        for j in i.cities:
+            print("\t{}: {}".format(j.id, j.name))
+    s.close()
