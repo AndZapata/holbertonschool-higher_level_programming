a3799924c9a1c7c614bb252a5594704802c6e32d
AndZapata <andzapata514@gmail.com>
2019-08-26 17:35:21 +0000

    [Fix] comment and lines

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-'''
-comment: takes in a URL, sends a request to the URL,
-displays the body of the response and handle the error code
-'''
+''' handle the error code '''
 if __name__ == "__main__":
     from urllib.request import Request, urlopen
     from urllib.error import HTTPError
     from sys import argv
-    url = argv[1]
-    req = Request(url)
+    req = Request(argv[1])
     try:
         with urlopen(req) as response:
             html = response.read()
