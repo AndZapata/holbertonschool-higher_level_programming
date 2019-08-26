49e03754a5416697c4db806a3b361a0b2c9c3c37
AndZapata <andzapata514@gmail.com>
2019-08-26 17:58:45 +0000

    [Fix] imports and structure

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 ''' Comment: fetches https://intranet.hbtn.io/status '''
 if __name__ == "__main__":
-    from urllib.request import urlopen
-    with urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(str(html))))
-        print('\t- content: {}'.format(html.decode('utf-8')))
+    from requests import get
+    html = get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type: {}'.format(type(html.text)))
+    print('\t- content: {}'.format(html.text))
