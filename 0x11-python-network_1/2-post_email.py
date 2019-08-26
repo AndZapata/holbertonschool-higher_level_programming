58cb092b649767bc02ee63fe7165f0381de3215d
AndZapata <andzapata514@gmail.com>
2019-08-26 17:01:15 +0000

    [Feature] takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+comment: takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response
+'''
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
+    url = argv[1]
+    value = {'email' : argv[2]}
+    data = parse.urlencode(value)
+    data = data.encode('utf-8')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        html = response.read()
+        print(html.decode('utf-8'))
