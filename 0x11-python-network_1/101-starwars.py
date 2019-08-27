e533d32028eb49b25d70bd85a9cebc2b0cbb5bbe
AndZapata <andzapata514@gmail.com>
2019-08-27 01:35:24 +0000

    [Fix] second page searching

--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -4,16 +4,16 @@ comment: takes in a URL and an email, sends a POST request to the passed URL
 with the email as a parameter, and displays the body of the response
 '''
 if __name__ == "__main__":
-    import requests
+    from requests import get, post
     from sys import argv
     url = 'https://swapi.co/api/people'
-    req = requests.post(url, params={'search': argv[1]})
+    req = post(url, params={'search': argv[1]})
     in_file = req.json()
     counter = in_file.get('count')
     print("Number of results: {}".format(counter))
     for answer in in_file.get('results'):
         print(answer.get('name'))
     while in_file.get('next'):
-        in_file = requests.get(in_file.get('next')).json()
+        in_file = get(in_file.get('next')).json()
         for answer in in_file.get('results'):
             print(answer.get('name'))
