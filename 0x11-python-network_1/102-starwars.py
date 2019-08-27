98bb08a65f82c276be305950baf39ae7f26a94d3
AndZapata <andzapata514@gmail.com>
2019-08-27 02:03:57 +0000

    [Feature] Starwars III

---
+++ 0x11-python-network_1/102-starwars.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+comment: takes in a URL and an email, sends a POST request to the passed URL
+with the email as a parameter, and displays the body of the response
+'''
+if __name__ == "__main__":
+    from requests import get, post
+    from sys import argv
+    url = 'https://swapi.co/api/people'
+    req = post(url, params={'search': argv[1]})
+    in_file = req.json()
+    counter = in_file.get('count')
+    print("Number of results: {}".format(counter))
+    for answer in in_file.get('results'):
+        print(answer.get('name'))
+        list_of_films = answer.get('films')
+        for i in list_of_films:
+            ans = get(i).json().get('title')
+            print("\t{}".format(ans))
+    while in_file.get('next'):
+        in_file = get(in_file.get('next')).json()
+        for answer in in_file.get('results'):
+            print(answer.get('name'))
+            list_of_films = answer.get('films')
+            for i in list_of_films:
+                ans = get(i).json().get('title')
+                print("\t{}".format(ans))
