a781576b7d58725b4b152c15755ba639b9ffe223
AndZapata <andzapata514@gmail.com>
2019-09-23 17:09:51 +0000

    [Fix] semistandard

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -6,17 +6,20 @@ class Rectangle {
       this.height = h;
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
-    };
+    }
   }
-  rotate() {
-    let temp = this.width;
+
+  rotate () {
+    const temp = this.width;
     this.width = this.height;
     this.height = temp;
   }
-  double() {
+
+  double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
   }
