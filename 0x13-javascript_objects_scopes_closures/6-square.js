5400fa750e49ea19f37ab38c8891b0c916a2beea
AndZapata <andzapata514@gmail.com>
2019-09-23 18:14:07 +0000

    [Feature] charprint method

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rec = require('./4-rectangle');
+
+class Square extends Rec {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c == null) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      console.log(c.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Square;
