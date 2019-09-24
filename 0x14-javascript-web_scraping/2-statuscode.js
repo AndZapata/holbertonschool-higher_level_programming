a55206cf4eac2ce82789dae92e27abc779dafafa
AndZapata <andzapata514@gamil.com>
2019-09-24 18:09:39 +0000

    [Feature] get status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 /* Import module fs to open and write a file */
 const request = require('request');
 const url = process.argv[2];
-request (url, function (error, response, body) {
- if (err) {
-  console.log(error);
- } else {
-  console.log(JSON.parse(body).name);
- }
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
